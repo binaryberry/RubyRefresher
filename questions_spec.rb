2ebc760bc5945f603c86bb83cb27c839a450e3d7
Tatiana Soukiassian <binaryberry@gmail.com>
2014-12-08 22:14:11 +0100

    Update questions_spec.rb

--- questions_spec.rb
+++ questions_spec.rb
@@ -110,7 +110,7 @@ describe 'the Friday test :)' do
     n = get_elements_until_greater_than_five [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
     expect(n).to eq [1, 3, 5, 4, 1, 2]
   end
-
+#### bonus questions
   # specify 'convert_array_to_a_hash' do
   #   n = convert_array_to_a_hash ['a', 'b', 'c', 'd']
   #   expect(n).to eq({'a' => 'b', 'c' => 'd'})
